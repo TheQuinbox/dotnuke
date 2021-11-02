469c0fce1721a89dd5c7ae25ba89822a3b32eab0
Quin Marilyn <quin.marilyn05@gmail.com>
2021-11-02 12:27:57 -0600

    Fixed it. Stupid Go, it wasn't line 15.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		os.Exit(1)
 	}
 	fileList := make([]string, 0)
-	e := filepath.Walk(os.args[1], func(path string, f os.FileInfo, err error) error {
+	e := filepath.Walk(os.Args[1], func(path string, f os.FileInfo, err error) error {
 		fileList = append(fileList, path)
 		return err
 	})
