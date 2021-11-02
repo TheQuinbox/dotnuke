82f281eee5ad3595a323307a258a80d090a09d83
Quin Marilyn <quin.marilyn05@gmail.com>
2021-11-02 12:21:33 -0600

    Slightly broken code.

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	fmt.Println("Test.")
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <path>\n", os.Args[0])
+		os.Exit(1)
+	}
+	fileList := make([]string, 0)
+	e := filepath.Walk(os.args[1], func(path string, f os.FileInfo, err error) error {
+		fileList = append(fileList, path)
+		return err
+	})
+	if e != nil {
+		panic(e)
+	}
+	for _, file := range fileList {
+		fmt.Println(file)
+	}
 }
