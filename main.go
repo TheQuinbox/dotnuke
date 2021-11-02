68dece19abe1ef449ff003227662dac7ac05e5f5
Quin Marilyn <quin.marilyn05@gmail.com>
2021-11-02 12:39:15 -0600

    Actually delete files, and show totals.

--- main.go
+++ main.go
@@ -7,22 +7,33 @@ import (
 	"strings"
 )
 
+var total, deleted, ignored int
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Printf("Usage: %s <path>\n", os.Args[0])
 		os.Exit(1)
 	}
 	fileList := make([]string, 0)
-	e := filepath.Walk(os.Args[1], func(path string, f os.FileInfo, err error) error {
+	err := filepath.Walk(os.Args[1], func(path string, f os.FileInfo, err error) error {
 		fileList = append(fileList, path)
 		return err
 	})
-	if e != nil {
-		panic(e)
+	if err != nil {
+		panic(err)
 	}
 	for _, file := range fileList {
+		total += 1
 		if strings.HasPrefix(filepath.Base(file), "._") {
-			fmt.Printf("Found a file to delete: %s\n", filepath.Base(file))
+			deleted += 1
+			err := os.Remove(file)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			ignored += 1
 		}
 	}
+	fmt.Printf("Done! Out of %d files, %d were deleted, and %d were kept.", total, deleted, ignored)
+	os.Exit(0)
 }
